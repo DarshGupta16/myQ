bcf9f863596ed558656dccee7d80080390c50503
Darsh Gupta <dg@darshgupta.com>
2020-11-03 10:09:26 +0530

    Demonstrating Functions

---
+++ index.js
@@ -0,0 +1,27 @@
+// Demonstrating functions
+console.log(myQ.printQ());
+console.log(myQ.dQ("No items in queue"));
+console.log(myQ.front("No items in queue"));
+console.log(myQ.isEmpty());
+
+// Actual Example
+console.log("\n \n People waiting for Ice Cream");
+// enQ
+myQ.enQ("Dan");
+myQ.enQ("Edwin");
+myQ.enQ("Ezra");
+myQ.enQ("Tom");
+myQ.enQ("Toby");
+myQ.enQ("Tintin");
+myQ.enQ("Frank");
+myQ.enQ("Joe");
+// printQ
+console.log(myQ.printQ());
+// front
+console.log(myQ.front());
+// isEmpty
+console.log(myQ.isEmpty("No people in queue"));
+// dQ
+console.log(myQ.dQ("No people in queue"));
+
+console.log(myQ.printQ());
