d8fa8bffd2ade11b18aaacab0db4a1c76db572c5
Darsh Gupta <dg@darshgupta.com>
2020-11-03 09:11:35 +0530

    Adding dQ function

--- myQ.js
+++ myQ.js
@@ -6,4 +6,12 @@ class myQ {
   enQ(item) {
     this.items.push(item);
   }
+
+  dQ(error = "Error: No items in Queue") {
+    if (this.items.length == 0) {
+      return error;
+    } else {
+      this.items.shift();
+    }
+  }
 }
